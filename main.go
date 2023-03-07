fa7705049de490db6876d69d879bd3c6c73ea854
Pascal Hartmann <pascal.hartmann@mailbox.org>
2023-03-07 19:33:44 +0100

    Update

--- main.go
+++ main.go
@@ -12,19 +12,30 @@ import (
 )
 
 const (
-	MSG_CACHE_SIZE       = 9 // 10-1
+	// Amount of messages that are being cached to find the most popular ones
+	MSG_CACHE_SIZE = 9 // 10-1
+
+	// Threshold of how many times the same message has to be in msgCache in order to get repeated
 	MSG_REPEAT_THRESHOLD = 3
-	COOLDOWN_SECONDS     = 3
-	BOT_USER             = "poenjoyer"
-	CHANNEL              = "quin69"
+
+	// Cooldown to ratelimit sending chat messages
+	COOLDOWN_SECONDS = 3
+
+	// Twitch user that is authenticated to use this bot
+	BOT_USER = "poenjoyer"
+
+	// Twitch channel that BOT_USER should connect to
+	CHANNEL = "quin69"
 )
 
 var (
-	msgCache    [MSG_CACHE_SIZE]string
-	c           int
-	client      = twitch.NewClient(BOT_USER, "oauth:uk443gieu4w9tk333q4v1pvljswrd9")
-	rLimiter    = rate.Sometimes{First: 1, Interval: COOLDOWN_SECONDS * time.Second}
-	cacheWarmed = false
+	msgCache              [MSG_CACHE_SIZE]string
+	c                     int
+	client                = twitch.NewClient(BOT_USER, "oauth:uk443gieu4w9tk333q4v1pvljswrd9")
+	mainRLimiter          = rate.Sometimes{First: 1, Interval: COOLDOWN_SECONDS * time.Second}
+	dunningKrugerRLimiter = rate.Sometimes{First: 1, Interval: COOLDOWN_SECONDS * time.Second}
+	cacheWarmed           = false
+	lastMessageSent       string
 
 	// Make sure that every ascii letter is lowercase
 	blacklist = []string{
@@ -41,9 +52,8 @@ var (
 		"kill",
 		"die",
 		"nambla",
-		"@",        // do not repeat reply andies
-		"boob",     // set a good example and do not perpetuate the booba
-		"partyhat", // temporary
+		"@",    // do not repeat reply andies
+		"boob", // set a good example and do not perpetuate the booba
 	}
 
 	dunning_kruger_slice = []string{
@@ -99,25 +109,28 @@ func repeatPopularMessages(message twitch.PrivateMessage) {
 
 	for k, v := range dupMsgs {
 		// When a certain message in the cache has reached the threshold, repeat it.
-		if v >= MSG_REPEAT_THRESHOLD && (len(k) < 200 && len(k) > 0) {
+		if v >= MSG_REPEAT_THRESHOLD && !(k == lastMessageSent) {
 
 			// Check the blacklist to avoid repeating certain messages
 			if containsBlacklistedWord(k) {
 				continue
 			}
 
-			log.Printf("REPEATED %s\n", k)
+			log.Printf("%s: %s\n", BOT_USER, k)
 			client.Say(CHANNEL, k)
+			lastMessageSent = k
 		}
 	}
 }
 
 func containsKeyword(msg twitch.PrivateMessage, keyword string, ignorePrefixedMessage bool) bool {
+	normalizedMsg := strings.ToLower(msg.Message)
+
 	if ignorePrefixedMessage {
-		return strings.Contains(strings.ToLower(msg.Message), keyword) && !strings.HasPrefix(msg.Message, "@") && !strings.HasPrefix(msg.Message, "!")
+		return strings.Contains(normalizedMsg, keyword) && !strings.HasPrefix(msg.Message, "@") && !strings.HasPrefix(msg.Message, "!")
 	}
 
-	return strings.Contains(strings.ToLower(msg.Message), keyword)
+	return strings.Contains(normalizedMsg, keyword)
 }
 
 func main() {
@@ -125,7 +138,7 @@ func main() {
 	client.OnPrivateMessage(func(message twitch.PrivateMessage) {
 
 		// save the message in Postgres
-		saveChatMessage(message)
+		// saveChatMessage(message)
 
 		// If msgCache is full, reset the counter
 		if c == MSG_CACHE_SIZE {
@@ -138,9 +151,11 @@ func main() {
 
 		// ---------------------------- Replies to specific messages ------------------------------
 		if containsKeyword(message, "danny", true) {
-			idx := rand.Intn(len(dunning_kruger_slice))
-			client.Say(CHANNEL, fmt.Sprintf("classic %s", dunning_kruger_slice[idx]))
-			log.Println("Dunning Kruger'd")
+			dunningKrugerRLimiter.Do(func() {
+				idx := rand.Intn(len(dunning_kruger_slice))
+				client.Say(CHANNEL, fmt.Sprintf("classic %s", dunning_kruger_slice[idx]))
+				log.Println("Dunning Kruger'd")
+			})
 		}
 		// ----------------------------------------------------------------------------------------
 
@@ -152,7 +167,7 @@ func main() {
 		cacheWarmed = true
 
 		// Participate in chat spam
-		rLimiter.Do(func() {
+		mainRLimiter.Do(func() {
 			repeatPopularMessages(message)
 		})
 
